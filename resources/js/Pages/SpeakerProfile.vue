080551d9da234568f835dd40e41f3675a66da4c8
Mathias Hansen <me@codemonkey.io>
2024-08-26 18:24:00 +0200

    Removed repetitive header

--- resources/js/Pages/SpeakerProfile.vue
+++ resources/js/Pages/SpeakerProfile.vue
@@ -18,12 +18,6 @@ defineProps({
 <template>
 
     <GuestLayout :title="speaker.name">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ speaker.name }}
-            </h2>
-        </template>
-
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
